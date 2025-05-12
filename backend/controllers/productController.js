120e7658193c301daaa2b68301820c786579f1b1
ainkay <ain.kay@kalvium.community>
2025-05-12 12:09:19 +0530

    created models for product order and user

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+const Product = require('../models/Product');
+
+// Create a new product
+const createProduct = async (req, res) => {
+  try {
+    const { name, price, description, stock } = req.body;
+    const product = new Product({ name, price, description, stock });
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get all products
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createProduct, getProducts };
