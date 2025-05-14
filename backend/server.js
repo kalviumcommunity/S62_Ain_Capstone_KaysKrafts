322adab7a8651516d668b05109c4a875b03beb2e
ainkay <ain.kay@kalvium.community>
2025-05-15 00:02:46 +0530

    edit

--- backend/server.js
+++ backend/server.js
@@ -35,5 +35,5 @@ app.get('/', (req, res) => {
 // Define the port and start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is runing on port ${PORT}`);
 });
